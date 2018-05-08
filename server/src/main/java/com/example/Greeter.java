6161bf6e78f4429824c556ba756b1df137d437df
bhavyahemanth <bhavyashree.012@gmail.com>
2018-05-08 20:05:30 +0530

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ /**
+   * @bhavya someone the name of person
+   * @return greetinf string
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
